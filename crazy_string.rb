2052fbd8a6ce1e53b5e4434b92ec133e2e0c2c42
katiejg <jng.katie@gmail.com>
2019-02-27 15:48:12 +0000

    Automatically backed up by Learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -1,6 +1,6 @@
 # write your method here
 
 def crazy_strings(a , b)
-"#{a}".reverse.upcase + " #{b}".swapcase.gsub("s", "z")
+return "#{a}".reverse.upcase + " #{b}".swapcase.gsub("s", "z")
 end
 puts crazy_strings("APPLE", "BB")
